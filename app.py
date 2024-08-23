be468c4ff43873f70c6734dc959d98e26af07472
Cristina Sofia Stemmer <cristinasstemmer@gmail.com>
2024-08-23 19:51:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,15 @@
 from flask import Flask, jsonify, send_from_directory
-import main  # Importe o script com sua lógica de coleta de dados
+import main
 
 app = Flask(__name__, static_folder='static')
 
 @app.route('/')
 def serve_html():
-    # Serve o index.html a partir da pasta 'static'
     return send_from_directory(app.static_folder, 'index.html')
 
 @app.route('/data')
 def get_data():
-    data = main.get_data()  # Supondo que você tem uma função em main.py que retorna os dados
+    data = main.get_data()  
     return jsonify(data)
 
 if __name__ == '__main__':
